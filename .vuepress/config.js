6bd9aa97b70dd09bf8c1f9d9c67b82460a4e4f0b
s0wnd <aimmvp@gmail.com>
2020-03-02 09:20:35 +0900

    Add image files

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,17 +1,25 @@
 module.exports = {
-    title: 'UI Fwk Documentationdddd',
-    description: 'Just Play   ddddd',
     themeConfig: {
+        logo: '/assets/img/favicon.ico',    // Navbar Logo
+        displayAllHeaders: true,     // Display Header LInks of All Pages
         nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'About me', link: 'https://github.com/aimmvp' },
+            { text: 'Home', link: '/'},
+            { text: '업무화면', link: '/category1/'},
+            { text: 'Layout구성', link: '/category2/'},
+            { text: '전체공통', link: '/category3/'},
+            { text: 'VuePress', link: 'https://vuepress.vuejs.org'},
         ],
+        lastUpdated: 'Last Upadted',
+        markdown: {
+            lineNumbers: true,
+        },
+        sidebarDepth: 2,
         sidebar: {
-            '/guide/': [
+            '/category1/' : [
                 '',
-                'frontend',
-                'backend'
+                'button',
+                'input',
+                'calendar',
             ]
         }
     }
